03985321a5108a0942898c16bf89a6ae6d6ed363
muhdhuz <muhd.huz@gmail.com>
2020-06-11 11:36:36 +0800

    updated myUtils

--- myUtils.py
+++ myUtils.py
@@ -3,6 +3,8 @@ import numpy as np
 
 import glob
 import os
+import sys
+import re
 
 #======================================================================
 
@@ -36,6 +38,7 @@ def plot_signal(result,start=None,figsize=(20,1),grid=False,logy=False,start_min
 		fig.savefig(savename + '.png', format='png', bbox_inches = 'tight')
 	else:
 		plt.show()
+	plt.close(fig)
 
 def save_obj(obj, dir, name):
 	"""to save an obj using pickle"""
@@ -77,6 +80,14 @@ def config_parser(config_file,namespace=False):
 			var = json.load(f)
 	return var
 
+def dictfilter(dic, keys):
+	return {key:dic[key] for key in keys}
+
+def print_both(file, *args):
+	"""prints and write to file simultaneously"""
+	toprint = ' '.join([str(arg) for arg in args])
+	print(toprint)
+	file.write(toprint+'\n')
 
 #timing
 #*************************************
@@ -84,16 +95,16 @@ class Timer:
 	"""class to find elapsed time"""
 	def __init__(self):
 		from datetime import datetime
-        self.cache = datetime.now()
+		self.cache = datetime.now()
 
-    def check(self):
-        now = datetime.now()
-        duration = now - self.cache
-        self.cache = now
-        return duration.total_seconds()
+	def check(self):
+		now = datetime.now()
+		duration = now - self.cache
+		self.cache = now
+		return duration.total_seconds()
 
-    def reset(self):
-        self.cache = datetime.now()
+	def reset(self):
+		self.cache = datetime.now()
 
 def time_taken(elapsed):
 	"""To format time taken in hh:mm:ss. Use with time.monotic() or Timer class"""
@@ -105,6 +116,47 @@ def mydate():
 	"""returns current date and time"""
 	from datetime import datetime
 	return (datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+
+class ProgressBar(object):
+	"""incrementing progress bar
+	Credits to: Romuald Brunet from https://stackoverflow.com/questions/3160699/python-progress-bar"""
+	DEFAULT = 'Progress: %(bar)s %(percent)3d%%'
+	FULL = '%(bar)s %(current)d/%(total)d (%(percent)3d%%) %(remaining)d to go'
+
+	def __init__(self, total, width=40, fmt=DEFAULT, symbol='=',
+				 output=sys.stderr):
+		assert len(symbol) == 1
+
+		self.total = total
+		self.width = width
+		self.symbol = symbol
+		self.output = output
+		self.fmt = re.sub(r'(?P<name>%\(.+?\))d',
+			r'\g<name>%dd' % len(str(total)), fmt)
+
+		self.current = 0
+
+	def __call__(self):
+		percent = self.current / float(self.total)
+		size = int(self.width * percent)
+		remaining = self.total - self.current
+		bar = '[' + self.symbol * size + ' ' * (self.width - size) + ']'
+
+		args = {
+			'total': self.total,
+			'bar': bar,
+			'current': self.current,
+			'percent': percent * 100,
+			'remaining': remaining
+		}
+		print('\r' + self.fmt % args, file=self.output, end='')
+		self.current += 1
+
+	def done(self):
+		self.current = self.total
+		self()
+		print('', file=self.output)
 
 
 #file/folder manipulation
@@ -134,15 +186,29 @@ def listDirectory(directory,fileExtList='.wav',regex=None):
 				if os.path.splitext(f)[1] in fileExtList]  
 	return fileList , fnameList
 
-def listDirectory_all(directory,topdown=True):
-	"""returns a list of all files in directory and all its subdirectories"""
-	fileList = []
-	fnameList = []
-	for root, _, files in os.walk(directory, topdown=topdown):
-		for name in files:
-			fileList.append(os.path.join(root, name))
-			fnameList.append(name)
-	return fileList , fnameList
+def listDirectory_all(directory,fileExt='.wav',topdown=True):
+    """returns a list of all files in directory and all its subdirectories
+	directory can also take a single file.
+    fileList: full path to file
+    fnameList: basenames
+    fnameList_noext: basenames with no extension"""
+    fileList = []
+    fnameList = []
+    fnameList_noext = []
+    if os.path.isdir(directory):
+        for root, _, files in os.walk(directory, topdown=topdown):
+            for name in files:
+                if name.endswith(fileExt):
+                    fileList.append(os.path.join(root, name))
+                    fnameList.append(name)
+                    fnameList_noext.append(os.path.splitext(name)[0])
+    else:
+        if directory.endswith(fileExt):
+            fileList.append(directory)
+            basename = os.path.basename(directory)
+            fnameList.append(basename)
+            fnameList_noext.append(os.path.splitext(basename)[0])       
+    return fileList, fnameList, fnameList_noext
 
 def mass_delete(directory,regex,topdown=True):
 	"""deletes all files matching regex in directory and all its subdirectories"""
@@ -154,13 +220,31 @@ def mass_delete(directory,regex,topdown=True):
 				os.remove(os.path.join(root, name))
 	return deleteList
 
+def find_classes(dir):
+	"""
+	Finds the class folders in a dataset.
+	input params: dir (string)- Root directory path.
+	Returns: tuple (classes, class_to_idx) - where classes are relative to (dir), and class_to_idx is a dictionary.
+	Ensures: No class is a subdirectory of another.
+	"""
+	if isinstance(dir, str):
+		if sys.version_info >= (3, 5):
+			# Faster and available in Python 3.5 and above
+			classes = [d.name for d in os.scandir(dir) if d.is_dir()]
+		else:
+			classes = [d for d in os.listdir(dir) if os.path.isdir(os.path.join(dir, d))]
+		classes.sort()
+		class_to_idx = {classes[i]: i for i in range(len(classes))}
+	elif isinstance(dir, (list,)):
+		classes = [i for i in range(len(dir))]
+		class_to_idx = {classes[i]: i for i in classes}
+	return classes, class_to_idx
 
 #nsynth dataset
 #*************************************
 def extract_nsynth_pitch(filename):
 	"""function to extract midi pitch from nsynth dataset base filenames
 	keyboard_acoustic_000-059-075.wav -> 59""" 
-	import re
 	n = re.findall(r'(?<=-).*?(?=-)', filename)[0]
 	if (n[0]=='0') :
 		midinum=int(n[1:])
@@ -171,7 +255,6 @@ def extract_nsynth_pitch(filename):
 def extract_nsynth_instrument(filename):
 	"""function to extract midi pitch from nsynth dataset base filenames
 	keyboard_acoustic_000-059-075.wav -> keyboard_acoustic""" 
-	import re
 	n = re.search('(.*?_.*?)_', filename).group(1)
 	return n
 
@@ -214,3 +297,10 @@ def ScaleAudio_all(indir,outdir,scaling_function,*args,**kwargs):
 				ScaleAudio(os.path.join(dirpath, name),structure,scaling_function,*args,**kwargs)
 
 
+#neural network training
+#*************************************
+def LinearScheduler(start,end,steps):
+	"""yields a value that decays linearly with steps.
+	used for scheduled sampling"""
+	for i in np.linspace(start,end,steps):
+		yield i
